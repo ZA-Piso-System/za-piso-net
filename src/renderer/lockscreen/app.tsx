0cc7cd5f123dc4a4daca05ea9e802ef64be50c21
jasonlerit <jasonlerit077@gmail.com>
2026-03-11 13:18:20 +0800

    feat: add tailwindcss

--- src/renderer/lockscreen/app.tsx
+++ src/renderer/lockscreen/app.tsx
@@ -1,7 +1,7 @@
 export default function App(): React.JSX.Element {
   return (
-    <div>
-      <div>TODO</div>
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-5xl text-purple-500">ZA Pisonet</div>
     </div>
   )
 }
--- src/renderer/lockscreen/main.tsx
+++ src/renderer/lockscreen/main.tsx
@@ -1,3 +1,5 @@
+import '../global.css'
+
 import App from '@renderer/lockscreen/app'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
