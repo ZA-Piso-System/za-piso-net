3dda95a039abd5b719498ca9d858ebaeddfee53a
jasonlerit <jasonlerit077@gmail.com>
2026-03-15 22:16:43 +0800

    feat: persist config in userData

--- src/main/config.ts
+++ src/main/config.ts
@@ -4,17 +4,18 @@ import path from 'path'
 import { AppConfig } from '../common/types/app.type'
 import { v4 as uuidv4 } from 'uuid'
 
-const configPath = app.isPackaged
+const userConfigPath = path.join(app.getPath('userData'), 'config.json')
+
+const defaultConfigPath = app.isPackaged
   ? path.join(process.resourcesPath, 'config.json')
   : path.join(process.cwd(), 'resources/config.json')
 
-const config: AppConfig = JSON.parse(fs.readFileSync(configPath, 'utf8'))
-
-if (!config.deviceId) {
-  config.deviceId = uuidv4()
-  if (process.env.NODE_ENV !== 'development') {
-    fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
-  }
+if (!fs.existsSync(userConfigPath)) {
+  const defaultConfig: AppConfig = JSON.parse(fs.readFileSync(defaultConfigPath, 'utf8'))
+  defaultConfig.deviceId = uuidv4()
+  fs.writeFileSync(userConfigPath, JSON.stringify(defaultConfig, null, 2))
 }
+
+const config: AppConfig = JSON.parse(fs.readFileSync(userConfigPath, 'utf8'))
 
 export default config
